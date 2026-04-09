0e685a88b2a9ae96506d0d691cac5734f3c22d5f
SENA-proyect <proyectofinanzassena@gmail.com>
2026-04-08 19:00:11 -0500

    Estructuracion de la parte del administrador, y visualizacion del usuario en el panel del administrador.

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { register, login, getUsuarios } = require("../controllers/authController");
+const { verifyToken } = require("../middlewares/authMiddleware")
 
 router.post("/Register", register);
 router.post("/Login", login);
-router.get("/PanelUsuarios", getUsuarios);
+router.get("/PanelUsuarios", verifyToken, getUsuarios);
 
 module.exports = router;
--- Frontend/src/components/PanelUsuarios.jsx
+++ Frontend/src/components/PanelUsuarios.jsx
@@ -1,10 +1,42 @@
-// src/components/PanelUsuarios.jsx
 import '../styles/panel.css'
+import { useState, useEffect } from 'react';
 
-export default function PanelUsuarios({ usuarios = [] }) {
+export default function PanelUsuarios() {
+  const [usuarios, setUsuarios] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getUsuarios();
+  }, []);
+
+  const getUsuarios = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      console.log('Token', token);
+      const response = await fetch('http://localhost:3000/api/auth/PanelUsuarios', {
+        headers: { 'Authorization': `Bearer ${token}` }
+      });
+      const data = await response.json();
+      
+      if (data.ok) {
+        setUsuarios(data.usuarios);
+      } else {
+        setError(data.mensaje);
+      }
+    } catch (err) {
+      setError('Error al obtener usuarios');
+      console.error(err);
+    } finally {
+      setCargando(false);
+    }
+  };
+
+  if (cargando) return <p>Cargando...</p>;
+  if (error) return <p className="error">{error}</p>;
+
   return (
-    <div>
-
+    <div style={{ minHeight: '100vh', background: 'linear-gradient(135deg, #0a0a0a, #0d1a0d)' }} >
       <div className="inf-container">
         <h2>Usuarios Registrados</h2>
 
@@ -29,7 +61,6 @@ export default function PanelUsuarios({ usuarios = [] }) {
           )}
         </div>
       </div>
-
     </div>
   )
 }
