73e45b32752c8e8e36663442f18b0020c696af94
SENA-proyect <proyectofinanzassena@gmail.com>
2026-04-08 20:57:56 -0500

    CRUD usuario finalizado, faltante autorizacion de usuario y cambio de contraseña, (IA Commit)

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, getUsuarios } = require("../controllers/authController");
-const { verifyToken } = require("../middlewares/authMiddleware")
+const { register, login, getUsuarios, updateUsuario, deleteUsuario } = require("../controllers/authController");
+const { verifyToken } = require("../middlewares/authMiddleware");
 
 router.post("/Register", register);
 router.post("/Login", login);
 router.get("/PanelUsuarios", verifyToken, getUsuarios);
-
+router.put("/PanelUsuarios/:id", verifyToken, updateUsuario);  // ← PanelUsuarios
+router.delete("/PanelUsuarios/:id", verifyToken, deleteUsuario);  // ← PanelUsuarios
 module.exports = router;
