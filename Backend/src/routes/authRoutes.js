a5d4e459220a40bd7d40ac360e661ae11232be89
SENA-proyect <proyectofinanzassena@gmail.com>
2026-04-09 23:33:29 -0500

    prueba

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/authController");
+const { verifyToken } = require("../middlewares/authMiddleware");
+const { register, login, getUsuarios } = require("../controllers/authController");
+
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/usuarios", verifyToken, getUsuarios);
 
-module.exports = router;
+module.exports = { register, login, getUsuarios };
