bfac88eb7bc5e4cd79dd756ca7d44d294fdbf8b4
Juanma-MG21 <juanma1608g@gmail.com>
2026-08-29 22:56:24 -0500

    arreglo de bugs en el panel de administrador

--- Frontend/src/components/PanelUsuarios.jsx
+++ Frontend/src/components/PanelUsuarios.jsx
@@ -33,7 +33,7 @@ export default function PanelUsuarios() {
       const token = localStorage.getItem("token");
 
       const response = await fetch(
-        "https://localhost:3000/api/auth/PanelUsuarios",
+        "http://localhost:3000/api/auth/PanelUsuarios",
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -83,7 +83,7 @@ export default function PanelUsuarios() {
     try {
       const token = localStorage.getItem('token');
       const response = await fetch(
-        `https://localhost:3000/api/auth/PanelUsuarios/${usuarioSeleccionado.ID_usuario}`,
+        `http://localhost:3000/api/auth/PanelUsuarios/${usuarioSeleccionado.ID_usuario}`,
         {
           method: 'PUT',
           headers: {
@@ -128,7 +128,7 @@ export default function PanelUsuarios() {
       try {
         const token = localStorage.getItem('token');
         const response = await fetch(
-          `https://localhost:3000/api/auth/PanelUsuarios/${id}`,
+          `http://localhost:3000/api/auth/PanelUsuarios/${id}`,
           {
             method: 'DELETE',
             headers: { 'Authorization': `Bearer ${token}` }
